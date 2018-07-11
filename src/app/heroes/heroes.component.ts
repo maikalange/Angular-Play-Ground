c27b1d24c29f264b5387820c889009d1df1283f2
Mai Kalange <joseph.nyirenda@gmail.com>
2018-07-11 16:08:59 +0100

    Resolved problems with even handler

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero} from './hero';
-import {HEROES} from '../mock-heroes';
+import {HEROES} from './mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-	hero: Hero={
-		id:1,
-		name:'Windstorm'
-	};
-	selectedHero:Hero;
-	onselect(hero:Hero):void{
-		this.selectedHero = hero;
-	}
  heroes = HEROES;
+	selectedHero:Hero;
+
+
   constructor() { }
 
   ngOnInit() {
 
   }
+  	onSelect(hero:Hero):void{
+		this.selectedHero = hero;
+	}
 
 }
